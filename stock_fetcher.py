39441161abf500813fc4958a55784f2888c839d3
Mihir Sawant <mihirssawant06@gmail.com>
2023-01-31 16:48:39 +0530

    python file added

--- stock_fetcher.py
+++ stock_fetcher.py
@@ -1,5 +1,12 @@
 import requests
-# r = requests.get('https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json')
-f = open("StockFetcher.java", "r")
-print(f.read())
-
+import json
+import os
+import time
+begin = time.time()
+r = requests.get('https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json')
+f = open("res_py.txt", "w")
+for i in r.json():
+    f.write(json.dumps(i))
+f.close()
+end = time.time()
+print(f"Finished Task: {end - begin}")
